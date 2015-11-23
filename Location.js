010e23f0af8193cded46333226db70ac32f2bd04
ElSergio217 <sergioa.garcia217@gmail.com>
2015-11-22 19:29:08 -0500

    Fist commit

---
+++ Location.js
@@ -0,0 +1,5 @@
+var Location = [
+    { 'name': 'Knowledge House', 'lat': 40.816638, 'lng': -73.890643 },
+    { 'name': 'Scynyc', 'lat': 40.810950, 'lng': -73.929081 },
+    { 'name': 'The Point', 'lat': 40.818243, 'lng': -73.890567 }
+];
