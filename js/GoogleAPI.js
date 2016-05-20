ea47466b797d52b3dfc99a6559fc4d9471cb02e7
Sergio <Sergioa.garcia217@gmail.com>
2016-05-20 19:32:47 -0400

    updated

--- js/GoogleAPI.js
+++ js/GoogleAPI.js
@@ -1,11 +1,39 @@
 var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 40.8248132, lng: -73.8989097 },
-        zoom: 14
-    });
+  var styles = [
+    {
+      stylers: [
+        { hue: "#00ffe6" },
+        { saturation: -20 }
+      ]
+    },{
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { lightness: 100 },
+        { visibility: "simplified" }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    }
+  ];
 
-    for (var x in Location) {
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
+	
+	map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 40.7889673, lng: -73.9201427 },
+        zoom: 12
+    });
+	
+	map.mapTypes.set('map_style', styledMap);
+	map.setMapTypeId('map_style');
+	
+	for (var x in Location) {
         var building = Location[x];
         var location = new google.maps.LatLng(building.lat, building.lng);
         addMarker(map, building.name, location);
@@ -15,7 +43,8 @@ function initMap() {
 function addMarker(map, name, location) {
     var marker = new google.maps.Marker({
         position: location,
-        map: map
+        map: map,
+		icon:"img/icon.png"
     });
 
     google.maps.event.addListener(marker, 'click', function () {
@@ -24,5 +53,6 @@ function addMarker(map, name, location) {
             content: name
         });
         infowindow.open(map, marker);
+		$("#myModal").modal();
     });
 }
--- js/Location.js
+++ js/Location.js
@@ -1,5 +1,7 @@
 var Location = [
-    { 'name': 'Knowledge House', 'lat': 40.816638, 'lng': -73.890643 },
+    { 'name': 'BXL Incubator', 'lat': 40.816638, 'lng': -73.890643 },
     { 'name': 'Scynyc', 'lat': 40.810950, 'lng': -73.929081 },
-    { 'name': 'The Point', 'lat': 40.818243, 'lng': -73.890567 }
+    { 'name': 'Ghetto Film School', 'lat': 40.8063976, 'lng': -73.9281422 },
+	{ 'name': 'American Museum of Natural History', 'lat': 40.7813241, 'lng': -73.9739882 },
+	{ 'name': 'BrainPOP', 'lat': 40.7429639, 'lng': -73.9922767 }
 ];
