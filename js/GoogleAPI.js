b3428be7a970c2f0bbd900e799975efdec76683f
ElSergio217 <sergioa.garcia217@gmail.com>
2016-05-20 20:07:15 -0400

    Location Array moved

--- js/GoogleAPI.js
+++ js/GoogleAPI.js
@@ -33,6 +33,14 @@ function initMap() {
 	map.mapTypes.set('map_style', styledMap);
 	map.setMapTypeId('map_style');
 	
+	var Location = [
+		{ 'name': 'BXL Incubator', 'lat': 40.816638, 'lng': -73.890643 },
+		{ 'name': 'Scynyc', 'lat': 40.810950, 'lng': -73.929081 },
+		{ 'name': 'Ghetto Film School', 'lat': 40.8063976, 'lng': -73.9281422 },
+		{ 'name': 'American Museum of Natural History', 'lat': 40.7813241, 'lng': -73.9739882 },
+		{ 'name': 'BrainPOP', 'lat': 40.7429639, 'lng': -73.9922767 }
+	];
+	
 	for (var x in Location) {
         var building = Location[x];
         var location = new google.maps.LatLng(building.lat, building.lng);
--- js/Location.js
+++
@@ -1,7 +0,0 @@
-var Location = [
-    { 'name': 'BXL Incubator', 'lat': 40.816638, 'lng': -73.890643 },
-    { 'name': 'Scynyc', 'lat': 40.810950, 'lng': -73.929081 },
-    { 'name': 'Ghetto Film School', 'lat': 40.8063976, 'lng': -73.9281422 },
-	{ 'name': 'American Museum of Natural History', 'lat': 40.7813241, 'lng': -73.9739882 },
-	{ 'name': 'BrainPOP', 'lat': 40.7429639, 'lng': -73.9922767 }
-];
