e5ca96eb5193945c26050d64dbb70d7987e90631
Sergio <Sergioa.garcia217@gmail.com>
2016-06-09 00:07:53 -0400

    files

--- js/GoogleAPI.js
+++ js/GoogleAPI.js
@@ -13,6 +13,8 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
+initMap();
+
 function initMap() {
   var styles = [
     {
@@ -94,32 +96,58 @@ function addMarker(map, name, location) {
 		for(i = 0; i < arr.length; i++) {
 			if(spot=="BXL"){
 				if(typeof arr[i].BXL!="undefined"){
-				holder +='<h5>"' + arr[i].BXL + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
-				console.log(spot);
+					holder +='<h5>"' + arr[i].BXL + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
+					document.getElementById("1").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/15.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("2").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/16.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("3").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/17.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
 				}
 			}
 			if(spot=="Scenyc"){
 				if(typeof arr[i].Scenyc!="undefined"){
-				holder += '<h5>"' + arr[i].Scenyc + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
-				console.log(spot);
+					holder += '<h5>"' + arr[i].Scenyc + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
+					document.getElementById("1").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/18.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("2").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/19.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("3").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/20.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
 				}
 			}
 			if(spot=="Ghetto Film School"){
 				if(typeof arr[i].GFS!="undefined"){
-				holder += '<h5>"' + arr[i].GFS + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
-				console.log(spot);
+					holder += '<h5>"' + arr[i].GFS + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
+					document.getElementById("1").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/7.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("2").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/8.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("3").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/9.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
 				}
 			}
 			if(spot=="American Museum of Natural History"){
 				if(typeof arr[i].AMNH!="undefined"){
-				holder += '<h5>"' + arr[i].AMNH + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
-				console.log(spot);
+					holder += '<h5>"' + arr[i].AMNH + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
+					document.getElementById("1").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/4.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("2").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/5.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("3").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/6.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
 				}
 			}
 			if(spot=="BrainPOP"){
 				if(typeof arr[i].BP!="undefined"){
-				holder += '<h5>"' + arr[i].BP + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
-				console.log(spot);
+					holder += '<h5>"' + arr[i].BP + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
+					document.getElementById("1").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/12.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("2").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/13.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("3").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/14.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+				}
+			}
+			if(spot=="NYU"){
+				if(typeof arr[i].NYU!="undefined"){
+					holder += '<h5>"' + arr[i].NYU + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
+					document.getElementById("1").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/3.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("2").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/11.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("3").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/10.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+				}
+			}
+			if(spot=="MS"){
+				if(typeof arr[i].MS!="undefined"){
+					holder += '<h5>"' + arr[i].MS + '"</h5>' + "<h5><a href='http://" + arr[i].Wordpress + "'>" + arr[i].Name + '</a></h5>'+'<br>';
+					document.getElementById("1").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/1.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("2").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/2.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
+					document.getElementById("3").src = 'https://firebasestorage.googleapis.com/v0/b/concrete-sol-113722.appspot.com/o/21.png?alt=media&token=a36e2f73-d12e-41d4-8fda-6020871fae1b';
 				}
 			}
 		}
